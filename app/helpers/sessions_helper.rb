3982bc15b26b25ca902e36422db34568c7918ca9
takedag <c9397bf8e78xl8u@gmail.com>
2022-09-21 03:40:19 +0000

    Finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,21 +4,42 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # 永続的セッションを記憶。（Userモデルを参照）
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
   
-  # セッションと@current_userを破棄。。
+  # 永続的セッションを破棄。
+  def forget(user)
+    user.forget # Userモデル参照
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # セッションと@current_userを破棄。
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
 
-  # 現在ログイン中のユーザーがいる場合オブジェクトを返。。
+  # 一時的セッションにいるユーザーを返す。
+  # それ以外の場合はcookiesに対応するユーザーを返す。
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
-  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返。。
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返す。
   def logged_in?
     !current_user.nil?
   end
